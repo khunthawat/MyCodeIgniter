766479a7a823dfeec2d0983abd2a617dbb272f7b
khunthawat <khunthawat@seafdec.org>
2018-05-03 14:07:11 +0700

    สร้าง controller สำหรับจัดการเรือประมง

---
+++ application/controllers/FishingVessel.php
@@ -0,0 +1,22 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class FishingVessel extends CI_Controller {
+
+    public function index()
+    {
+        echo 'Fishing Vessel';
+    }
+    public function all()
+    {
+        echo 'Show All Vessels'
+    }
+
+}
+
+/* End of file FishingVessel.php */
+
+
+?>
