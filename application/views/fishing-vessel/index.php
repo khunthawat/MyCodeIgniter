736cd2bd1804ec4eda0d3fd7fbfaeb517cd3f710
khunthawat <khunthawat@seafdec.org>
2018-05-04 09:51:49 +0700

    กำหนดระยะคำบนหน้าจอแบบ 12 col เพื่อ Hello fisherman อยู่ตำแหน่งกลาง

---
+++ application/views/fishing-vessel/index.php
@@ -0,0 +1,5 @@
+<div class="row">
+    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color:green"></div>
+    <h1 class="col-lg-4 col-sm-12 col-md-10">Hello, FIshermen.</h1>
+    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color:green"></div>
+</div>
